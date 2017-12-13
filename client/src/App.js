521cfb488ae07ae8ae3d1d42475595393e66cd4f
misterfoxy <michaelscottfox1@gmail.com>
2017-12-12 21:31:34 -0800

    forgot to add dashboard to wireframe

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-ro
 import About from "./pages/About"
 import Login from "./pages/Login"
 import Order from "./pages/Order"
+import Dashboard from "./pages/Dashboard"
 import Nav from "./components/Nav";
 
 
@@ -14,6 +15,7 @@ const App = () =>
         <Route exact path="/" component={About} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/order" component={Order} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </div>
   </Router>;
---
+++ client/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Dashboard.css";
+
+class Dashboard extends React.Component {
+
+    render(){
+        return(
+            <h1>Dashboard</h1>
+        );
+    }
+}
+
+export default Dashboard;
